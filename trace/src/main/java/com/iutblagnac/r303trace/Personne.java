c01694904375f4c8870fdfa3eb27cf66af38a56d
Dresseur_Panda <mathias.garaios@gmail.com>
2025-09-18 12:25:05 +0200

    Modification du pomxml pour mettre à jour junit, ajout des test , passage des test de la classe personne et modification de la classe personne pour qu'il retourne des string au lieu de les afficher

--- trace/src/main/java/com/iutblagnac/r303trace/Personne.java
+++ trace/src/main/java/com/iutblagnac/r303trace/Personne.java
@@ -6,28 +6,43 @@ public class Personne {
     String prenom ;
     boolean travail;
 
-
+   
+    /**
+     * Constructeur par défaut
+     */
     public Personne(){
         this.nom = "Pascal" ;
         this.prenom = "Bob" ;
         this.travail = false ;
     }
 
+    /**
+     * Constructeur avec paramètres
+     * @param pNom
+     * @param pPrenom
+     */
     public Personne( String pNom,  String pPrenom){
         this.nom = pNom ;
         this.prenom = pPrenom ;
         this.travail = false ;
     }
 
-    public void presenter(){
-        if (this.travail)
-            System.out.println("Je suis " + prenom + "\n" + //
+    /**
+     * Présente la personne si elle travaille ou pas
+     * @return String
+     */
+    public String presenter(){
+        if (!this.travail)
+            return "Je suis " + prenom + "\n" + //
                         "Je suis " + prenom + "\n" + //
-                        "Je suis " + prenom + ", je suis " + prenom + " " + nom);
+                        "Je suis " + prenom + ", je suis " + prenom + " " + nom;
         else 
-            System.out.println("Me déranger pas je bricole, -_-");
+            return "Me déranger pas je bricole, -_-";
     }
 
+    /**
+     * Défini si la personne travaille ou pas
+     */
     public void travailler(){
         if (!travail){
             System.out.println(prenom + "va au travail !");
